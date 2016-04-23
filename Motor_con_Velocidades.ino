4eaadb27b9d9a25e360edaeafb67364a582dde9e
DanyCreed <dtoledop93@gmail.com>
2016-04-22 23:42:34 -0500

    Added files via upload

---
+++ Motor_con_Velocidades.ino
@@ -0,0 +1,12 @@
+int In = A2;
+int valor = 0;
+int transistor = 3;
+void setup(){
+  pinMode(transistor, OUTPUT);
+  
+}
+
+void loop(){
+  valor = analogRead(In);
+  analogWrite(transistor, valor/5);
+}
